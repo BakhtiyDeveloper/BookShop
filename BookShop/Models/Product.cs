54200fddb4a6ea37c21351e8f324b48e3b92786b
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-09-30 20:57:06 +0500

    Add project files.

---
+++ BookShop/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Models
+{
+    public class Product
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+    }
+}
