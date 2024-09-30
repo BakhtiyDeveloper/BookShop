03979ac7ada636e16cf754afde1522120086fe7a
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-09-30 22:09:29 +0500

    Add Migration

---
+++ BookShop/Migrations/20240930170358_FirstBookDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstBookDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.BookId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- BookShop/Models/Product.cs
+++ BookShop/Models/Product.cs
@@ -1,7 +1,11 @@
-﻿namespace BookShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
 {
     public class Product
     {
+
+        [Key]
         public int BookId { get; set; }
         public string BookName { get; set; }
         public string? AuthorName { get; set; }
